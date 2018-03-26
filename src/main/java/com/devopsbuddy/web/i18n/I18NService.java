94383fa6821f2692a39b45397869e53f44ba91fb
Sahil Chaudhary <sahil.chaudhary987@gmail.com>
2018-03-27 02:46:18 +0530

    Added logging set up

--- src/main/java/com/devopsbuddy/web/i18n/I18NService.java
+++ src/main/java/com/devopsbuddy/web/i18n/I18NService.java
@@ -1,7 +1,11 @@
 package com.devopsbuddy.web.i18n;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
 import java.util.Locale;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -9,11 +13,15 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class I18NService {
-
+	
+	
+	private static final Logger LOG = LoggerFactory.getLogger(I18NService.class);
+	
 	@Autowired
 	private MessageSource messageSource;
 
 	public String getMessage(String messageId) {
+		LOG.info("returning i18n text for messageid {}",messageId);
 		Locale locale = LocaleContextHolder.getLocale();
 		return getMessage(messageId, locale);
 	}
